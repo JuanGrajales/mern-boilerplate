7b76279fbc7132031057e75fe140a605fba0b34e
Juan Grajales <grajales.juanfe11@gmail.com>
2020-02-12 23:04:21 -0500

    commenting-express-generator

--- express-server/app.js
+++ express-server/app.js
@@ -8,7 +8,7 @@ const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
 const app = express();
-
+// test
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
