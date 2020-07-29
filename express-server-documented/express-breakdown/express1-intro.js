cc23df7d6e1fc13c951f2166500b49377d6aa451
JuanGrajales <inbox1.juan+github@gmail.com>
2020-07-28 20:14:51 -0400

    updated backend

--- express-server-documented/express-breakdown/express1-intro.js
+++ express-server-documented/express-breakdown/express1-intro.js
@@ -1,14 +1,31 @@
 /**
- * Node.js is a low-level platform; thus, many frameworks (e.g. Express) were built to enhance Node.js.
- * See some of the popular Node.js frameworks here https://nodejs.dev/learn.
- */ 
+ * What is Node.js?
+ * Node.js is an open, free source server environment. Node.js uses JS on the server-side
+ */
 
 /**
- * The Express constant is a module. With this you can call express() (a top level function) and other methods.
- * Before importing (i.e. require) Express you have to install it (npm install Express).
- * Express also comes with some useful methods (see https://expressjs.com/en/api.html#express).
- */ 
-const express = require('express'); 
+ * Why use Node.js? What can Node.js do?
+ * https://www.w3schools.com/nodejs/nodejs_intro.asp
+ */
+
+/**
+ * Many frameworks (e.g. Express.js) were built to enhance Node.js.
+ * Express.js did for Node.js what Bootstrap did for HTML/CSS and responsive web design.
+ * See some of the popular Node.js frameworks here https://nodejs.dev/learn.
+ */
+
+/**
+ * How to run a Node.js file.
+ * In the command line run $ node name-of-file
+ */
+
+/**
+ * Express.js is a minimal and flexible Node.js framework.
+ * Before importing (i.e. require) Express.js you have to install it (npm install express).
+ * Express.js also comes with many useful methods (see https://expressjs.com/en/api.html#express).
+ */
+
+const express = require("express");
 
 /**
  * The app constant is the actual Express app (it is an instance of Express).
@@ -18,31 +35,32 @@ const express = require('express');
  *    - Methods for rendering HTML views
  *    - Methods for registering a template engine
  *    - Settings (i.e properties) that affect how the app behaves
- * We will talk about all these properties more in depth later. 
+ * We will talk about all these properties more in depth later.
  * Make sure you import Express before actually calling express().
  * Learn more at https://expressjs.com/en/api.html#app.
- */ 
-const app = express();   
+ */
+
+const app = express();
 
 /**
  * While it is not necessary, I find it helpful to set the port and host as a constant.
  * Later on if you decide to change the port or host you can change the value of the constant
- * instead of refactor.
+ * instead of refactoring.
  */
-const port = 5000
-const host = 'localhost'
+const port = 5000;
+const host = "localhost";
 
 /**
  * Routing refers to determining how an application responds to a client request to a particular endpoint.
  * An endpoint is a URI (or path) and a specific HTTP request method (e.g. GET, POST, etc).
- * In the example below a client makes a get request to the / path and the server responds with "Home route".
+ * In the example below a client makes a GET request to the / path and the server responds with "Home route".
  * The req object represents the HTTP request and has many properties that will be covered later.
  * Learn more at https://expressjs.com/en/api.html#req.
  * The res object represents the HTTP response that an Express app sends when it gets an HTTP request.
  * Learn more at https://expressjs.com/en/api.html#res.
  * In this example we are using the send method, which is a property of the res object.
  */
-app.get('/', (req, res) => {  
+app.get("/", (req, res) => {
   res.send(`Home route`);
 });
 
@@ -52,4 +70,6 @@ app.get('/', (req, res) => {
  * The second parameter (host) is optional and if you omit it the default will be "localhost"
  * The third parameter (callback) is also optional; however, it's often used to let the dev know server is running correctly.
  */
-app.listen(port, host, () => console.log(`Example app listening at http://${host}:${port}`))
+app.listen(port, host, () =>
+  console.log(`Example app listening at http://${host}:${port}`)
+);
--- express-server-documented/express-breakdown/express2-path.js
+++ express-server-documented/express-breakdown/express2-path.js
@@ -1,29 +1,31 @@
 /******************** Modules ********************/
-const express   = require('express'); 
-const app       = express();
+const express = require("express");
 
 /**
- * The Node path module doens't require npm install path because it's part of Node's core.
- * The path module provides useful ways to access and interact with the file system.
+ * The Node.js path module doens't require npm install path because it comes built in with Node.js.
+ * The path module provides useful ways to work with file and directory paths.
  * Check the documentation here: https://nodejs.org/api/path.html.
  */
-const path      = require('path');
+const path = require("path");
 
 /******************** Constants ********************/
-const port = 5000
-const host = 'localhost'
+const app = express();
+const port = 5000;
+const host = "localhost";
 
 /**
  * Examples of methods given by the path module
  */
-console.log(path.basename('/testing/test/something')) // Output: something
+console.log(path.basename("/testing/test/something")); // Output: something
 
 /******************** Middleware ********************/
 
 /******************** Routes ********************/
-app.get('/', (req, res) => {  
+app.get("/", (req, res) => {
   res.send(`Home route`);
 });
 
 // This is not a route. Here we just specify which port the server will listen on.
-app.listen(port, host, () => console.log(`Example app listening at http://${host}:${port}`))
+app.listen(port, host, () =>
+  console.log(`Example app listening at http://${host}:${port}`)
+);
